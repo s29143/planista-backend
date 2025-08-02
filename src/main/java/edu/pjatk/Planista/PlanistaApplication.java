7201fd554772979affdaa5e93bfd8d74ede9c228
s29143 <s29143@pjwstk.edu.pl>
2025-08-02 21:43:39 +0200

    Initial commit

---
+++ src/main/java/edu/pjatk/Planista/PlanistaApplication.java
@@ -0,0 +1,13 @@
+package edu.pjatk.Planista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pjatk/Planista/PlanistaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pjatk.Planista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
