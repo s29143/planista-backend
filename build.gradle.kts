ef5819765b4173d7b6f77939ab37cca3be2cce2f
s29143 <s29143@pjwstk.edu.pl>
2025-09-04 01:01:54 +0200

    Added test containers for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,9 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("org.testcontainers:junit-jupiter:1.21.3")
+    testImplementation("org.testcontainers:postgresql:1.21.3")
 }
 
 tasks.withType<Test> {
--- src/test/java/edu/pjatk/planista/PlanistaApplicationTests.java
+++ src/test/java/edu/pjatk/planista/PlanistaApplicationTests.java
@@ -2,9 +2,26 @@ package edu.pjatk.planista;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class PlanistaApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:17.6");
+
+    @DynamicPropertySource
+    static void registerProps(DynamicPropertyRegistry r) {
+        r.add("spring.datasource.url", postgres::getJdbcUrl);
+        r.add("spring.datasource.username", postgres::getUsername);
+        r.add("spring.datasource.password", postgres::getPassword);
+    }
 
 	@Test
 	void contextLoads() {
