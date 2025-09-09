10914fbb4cde6a6953a3a878dcb927c2e7bc5fb1
s29143 <s29143@pjwstk.edu.pl>
2025-09-10 00:01:42 +0200

    Added auditable super class

---
+++ src/main/java/edu/pjatk/planista/config/Auditable.java
@@ -0,0 +1,35 @@
+package edu.pjatk.planista.config;
+
+import edu.pjatk.planista.auth.AppUser;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column
+    private Instant updatedAt;
+
+    @CreatedBy
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by", updatable = false)
+    private AppUser createdBy;
+
+    @LastModifiedBy
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "updated_by")
+    private AppUser updatedBy;
+}
