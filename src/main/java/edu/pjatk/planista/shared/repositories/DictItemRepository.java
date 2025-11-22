ef475f6889468fb5659b85c86ea1c5499083f502
s29143 <s29143@pjwstk.edu.pl>
2025-11-22 22:02:17 +0100

    Handling searching for dict items

--- src/main/java/edu/pjatk/planista/shared/repositories/DictItemRepository.java
+++ src/main/java/edu/pjatk/planista/shared/repositories/DictItemRepository.java
@@ -1,12 +1,12 @@
 package edu.pjatk.planista.shared.repositories;
 
 import edu.pjatk.planista.shared.models.DictItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
-import java.util.Optional;
-
 @NoRepositoryBean
 public interface DictItemRepository<T extends DictItem> extends JpaRepository<T, Long> {
-    Optional<T> findByNameIgnoreCase(String name);
+    Page<T> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
