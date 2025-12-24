41aefd622412dc966b554346f0ce0b8e08f2699e
s29143 <s29143@pjwstk.edu.pl>
2025-12-24 23:16:13 +0100

    Changed table to avoid problems with reserved keyword

--- src/main/java/edu/pjatk/planista/order/models/Order.java
+++ src/main/java/edu/pjatk/planista/order/models/Order.java
@@ -12,6 +12,7 @@ import java.time.LocalDate;
 @Entity
 @Getter
 @Setter
+@Table(name = "commission")
 public class Order extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
